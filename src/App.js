ed43298af42978959e89abc344d6692ca1d8a0dd
upsydedown <chintansenapati@gmail.com>
2024-05-21 11:02:59 +0530

    Minor Fixes, Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import "./App.css";
-
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 
 const App = () => {
+
+  const [ isOpen, setIsOpen ] = useState(true);
+  const navigate = useNavigate();
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleNavigate = (event) => {
+    if (event.target.classList.contains('modal-content')) return;
+    navigate('/about');
+  };
+
   const location = useLocation();
   const [ backgroundColor, setBackgroundColor ] = useState('white'); // Default color
   const [ logo, setLogo ] = useState('../images/logo_light.png'); // Default logo
@@ -42,11 +54,17 @@ const App = () => {
           {/* <div className="NavigationPanel">
             <Navbar />
           </div> */}
-          <div className="PagesPanel">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-            </Routes>
+
+          <div className={`modal ${isOpen ? 'active' : ''}`} onClick={handleNavigate}>
+            <div className="modal-content">
+              <div className="PagesPanel">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                </Routes>
+              </div>
+              {/* <button onClick={handleClose}>Close Modal</button> */}
+            </div>
           </div>
         </div>
       </section>
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,95 +4,78 @@ import "./Home.css";
 
 const Home = () => {
 
-    const [ isOpen, setIsOpen ] = useState(true); // Initially open
-    const navigate = useNavigate();
 
-    const handleClose = () => {
-        setIsOpen(false);
-    };
-
-    const handleNavigate = (event) => {
-        // Check if clicked outside specific content area (e.g., modal content)
-        if (event.target.classList.contains('modal-content')) return;
-        navigate('/about'); // Replace with your actual next page path
-    };
     return (
         <>
-            <section className="homeMain">
-                <div className={`modal ${isOpen ? 'active' : ''}`} onClick={handleNavigate}>
-                    <div className="modal-content">
-                        <div className="flex homeBody">
-                            <div className="flex dynamicLines" id="dynamicLines1">
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">We</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__thirdLine">
-                                    <h1 className="dynamicLines__text">Organize The</h1>
-                                </div>
-                            </div>
+            <section className="flex homeMain">
 
-                            <div className="flex dynamicLines" id="dynamicLines2">
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Connection</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                            </div>
-
-                            <div className="flex dynamicLines" id="dynamicLines3">
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Between</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Music</h1>
-                                </div>
-                            </div>
-
-                            <div className="flex dynamicLines" id="dynamicLines4">
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Artist</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Culture</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                            </div>
-
-                            <div className="flex dynamicLines" id="dynamicLines5">
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text">Art</h1>
-                                </div>
-                                <div className="dynamicLines__secondLine">
-                                    <img src="" alt="" srcset="" />
-                                </div>
-                                <div className="dynamicLines__firstLine">
-                                    <h1 className="dynamicLines__text"> & Collections</h1>
-                                </div>
-                            </div>
-
+                <div className="flex homeBody">
+                    <div className="flex dynamicLines" id="dynamicLines1">
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">We</h1>
                         </div>
-                        {/* <button onClick={handleClose}>Close Modal</button> */}
+                        <div className=" line antibar dynamicLines__secondLine">
+                            <img className="" src="./images/Home/screen_01/01.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__thirdLine">
+                            <h1 className="dynamicLines__text">Organize The</h1>
+                        </div>
                     </div>
-                </div>
 
+                    <div className="flex dynamicLines" id="dynamicLines2">
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Connection</h1>
+                        </div>
+                        <div className="line antibar dynamicLines__secondLine">
+                            <img src="./images/Home/screen_01/02.png" alt="" srcset="" />
+                        </div>                    </div>
+
+                    <div className="flex dynamicLines" id="dynamicLines3">
+                        <div className="line bar dynamicLines__secondLine">
+                            <img src="./images/Home/screen_02/03.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Between</h1>
+                        </div>
+                        <div className="dynamicLines__secondLine">
+                            <img src="./images/Home/screen_02/04.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Music</h1>
+                        </div>
+                    </div>
+
+                    <div className="flex dynamicLines" id="dynamicLines4">
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Artist</h1>
+                        </div>
+                        <div className="line antibar dynamicLines__secondLine">
+                            <img src="./images/Home/screen_01/05.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Culture</h1>
+                        </div>
+                        <div className="line bar dynamicLines__secondLine">
+                            <img src="./images/Home/screen_02/06.png" alt="" srcset="" />
+                        </div>
+                    </div>
+
+                    <div className="flex dynamicLines" id="dynamicLines5">
+                        <div className="line bar dynamicLines__secondLine">
+                            <img src="./images/Home/screen_01/07.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text">Art</h1>
+                        </div>
+                        <div className=" dynamicLines__secondLine">
+                            <img src="./images/Home/screen_02/08.png" alt="" srcset="" />
+                        </div>
+                        <div className="dynamicLines__firstLine">
+                            <h1 className="dynamicLines__text"> & Collections</h1>
+                        </div>
+                    </div>
+
+                </div>
 
             </section >
         </>
